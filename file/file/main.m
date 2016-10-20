1fbf7aea6b2a7dbc6fe00e4579aa1d57786d6121
ee <sunlixin@eedemacbook-air.local>
2016-10-20 11:08:08 +0800

    nihao

---
+++ file/file/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  file
+//
+//  Created by ee on 16/10/20.
+//  Copyright © 2016年 ee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
